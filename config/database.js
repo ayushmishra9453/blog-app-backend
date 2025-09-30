4b7fd466720054257ba71439de82233a9ffe4aa7
ayushmishra9453 <ayushmishra9453@gmail.com>
2025-09-30 19:47:00 +0530

    resume-filter plugin added

--- config/database.js
+++ config/database.js
@@ -1,23 +1,3 @@
-// const mongoose=require('mongoose');
-
-// require("dotenv").config();
-
-// const connectwithDb=()=>{
-//     mongoose.connect(process.env.DATABASE_URL,{
-//         useNewUrlParser:true,
-//         useUnifiedTopology:true
-//     })
-//     .then(() => {
-//     console.log('DB connected successfully');
-//   })
-//     .catch((error)=>{
-//         console.log("DB facing connection issue")
-//         console.log(error)
-//         process.exit(1)
-//     })
-// };
-// module.exports=connectwithDb;
-
 
 
 const mongoose = require('mongoose');
--- index.js
+++ index.js
@@ -1,68 +1,3 @@
-// const express=require('express')
-// const app=express();
-// const resumePlugin = require('../resume-filter-plugin');
-// require("dotenv").config();
-
-// const connectwithDb=require("./config/database")
-// connectwithDb();
-// const PORT=process.env.PORT || 3000;
-
-// app.use(express.json());
-
-// const blog=require("./routes/blog")
-// app.use('/api/resumes',resumePlugin);
-
-// mount 
-// app.use('/api/v1',blog);
-
-// app.listen(PORT,()=>{
-//     console.log(`api is running successfully- ${PORT}`);
-// })
-
-// app.get("/",(req,res)=>{
-//     res.send(`<h1>This is my home page</h1>`)
-// })
-
-
-// const app = express();
-// const resumePlugin = require('../resume-filter-plugin');
-// require("dotenv").config();
-// const connectwithDb = require("./config/database");
-// const PORT = process.env.PORT || 3000;
-// app.use(express.json());
-// const blog = require("./routes/blog");
-// connectwithDb()
-//   .then(() => {
-//     app.use('/api/resumes', resumePlugin);
-//     app.use('/api/v1', blog);
-//     app.get("/", (req, res) => {
-//       res.send(`<h1>This is my home page</h1>`);
-//     });
-//     app.listen(PORT, () => {
-//       console.log(`API is running successfully on port ${PORT}`);
-//     });
-//   })
-//   .catch((err) => {
-//     console.error("Failed to connect to DB", err);
-//   });
-
-
-// connectwithDb()
-//   .then((mongooseInstance) => {
-//     // If your plugin can accept mongoose instance, pass it here
-//     const resumePlugin = require('../resume-filter-plugin')(mongooseInstance);
-//     app.use('/api/resumes', resumePlugin);
-//     app.use('/api/v1', blog);
-//     app.get("/", (req, res) => {
-//       res.send(`<h1>This is my home page</h1>`);
-//     });
-//     app.listen(PORT, () => {
-//       console.log(`API is running successfully on port ${PORT}`);
-//     });
-//   })
-//   .catch((err) => {
-//     console.error("Failed to connect to DB", err);
-//   });
 
 
 const express=require('express')
@@ -74,8 +9,8 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 connectwithDb()
   .then((mongooseInstance) => {
-    // Import plugin and pass mongoose instance
-    const resumePlugin = require('../resume-filter-plugin')(mongooseInstance);
+   
+    const resumePlugin = require('../resume-filter-plugin/index')(mongooseInstance);
     app.use('/api/resumes', resumePlugin);
     app.use('/api/v1', blog);
     app.get("/", (req, res) => {
